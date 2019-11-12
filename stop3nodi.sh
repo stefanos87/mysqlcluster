f899e54c86b717ee438bfdd5e9ea404cd8adb6cd
stefanos87 <stefano.stirati@gmail.com>
2019-11-12 10:51:18 +0100

    Update stop3nodi.sh

--- stop3nodi.sh
+++ stop3nodi.sh
@@ -5,3 +5,6 @@ docker rm node1 node2 node3
 sudo rm -rf d1 d2 d3
 docker network rm groupnet
 # docker rmi stefanos87/sugar9mysql57:1.0
+docker rmi -f stefanos87/mysqlclusterdb1
+docker rmi -f stefanos87/mysqlclusterdb2
+docker rmi -f stefanos87/mysqlclusterdb3
